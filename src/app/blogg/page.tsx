e29f38eeaf9288ecff87fdc22a56a895ca1c1c0b
hetleo <hetleo@gmail.com>
2026-05-04 16:45:46 +0200

    Fixed next.js handeling of rendering

--- src/app/blogg/page.tsx
+++ src/app/blogg/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import { prisma } from "@/lib/db";
 import TopNav from "@/components/layout/TopNav";
--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import TopNav from "@/components/layout/TopNav";
 import PageShell from "@/components/layout/PageShell";
 import SectionLabel from "@/components/ui/SectionLabel";
